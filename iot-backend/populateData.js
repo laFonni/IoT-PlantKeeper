221cca7398342fe1990b58ca5d8deb575a4b153a
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2025-01-12 11:55:42 +0200

    Added userId assignment to new devices and expanded populateData.js

--- iot-backend/populateData.js
+++ iot-backend/populateData.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
+const bcrypt = require('bcrypt');
 
 const db = new sqlite3.Database('./iot.db', (err) => {
   if (err) {
@@ -9,8 +10,53 @@ const db = new sqlite3.Database('./iot.db', (err) => {
   }
 });
 
-function populateDatabase() {
-  const deviceId = 1; // Example device ID
+async function populateDatabase() {
+  const hashedPassword = await bcrypt.hash('1234qwer', 10);
+  const userId = await new Promise((resolve, reject) => {
+    db.run(
+      'INSERT INTO Users (email, password) VALUES (?, ?)',
+      ['john.doe@example.com', hashedPassword],
+      function (err) {
+        if (err) {
+          console.error('Failed to insert user:', err);
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      }
+    );
+  });
+
+  const wifiId = await new Promise((resolve, reject) => {
+    db.run(
+      'INSERT INTO WiFiNetworks (ssid, password, userId) VALUES (?, ?, ?)',
+      ['Galaxy A15 57B3', 'wifiPassword', userId],
+      function (err) {
+        if (err) {
+          console.error('Failed to insert WiFi network:', err);
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      }
+    );
+  });
+
+  const deviceId = await new Promise((resolve, reject) => {
+    db.run(
+      'INSERT INTO IoTDevices (name, macAddress, wifiId, userId) VALUES (?, ?, ?, ?)',
+      ['IoT Device 1', '00:1B:44:11:3A:B7', wifiId, userId],
+      function (err) {
+        if (err) {
+          console.error('Failed to insert IoT device:', err);
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      }
+    );
+  });
+
   const sensorTypes = ['soilMoisture', 'temperature', 'lightLevel'];
   const now = new Date();
 
@@ -33,4 +79,4 @@ function populateDatabase() {
     }
     db.run('COMMIT');
   });
-} 
+}
